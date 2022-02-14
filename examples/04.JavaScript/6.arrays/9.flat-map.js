f7330bb85962950cebcaf88530afcfb79b4e79b7
Baraa-bt1704393 <97453805+Baraa-bt1704393@users.noreply.github.com>
2022-02-14 22:34:11 +0300

    javaScript

--- examples/04.JavaScript/6.arrays/9.flat-map.js
+++
@@ -1,6 +0,0 @@
-let books = [
-    {title: "Head First JavaScript", authors: ["Jhon Doe", "Mr Bean"]},
-    {title: "JavaScript in Action", authors: ["Said Saad", "Cool Guy"]}
-]
-let authors = books.flatMap(b => b.authors);
-console.log(authors);
